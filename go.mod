56ce72f133e45b3023200b7b7aebbe5ede69cdb2
David Saah <dasorange.hope@gmail.com>
2023-10-18 22:44:46 +0000

    create http server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/DaveSaah/voting-system
 
 go 1.21.3
+
+require github.com/go-sql-driver/mysql v1.7.1 // indirect
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/DaveSaah/voting-system/handlers"
+)
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+	http.HandleFunc("/dashboard", handlers.Dashboard)
+	http.HandleFunc("/login", handlers.Login)
+
+	log.Println("Listening on :8080")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
