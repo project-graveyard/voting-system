f3a7df3cd0d3e39ca31487fd27f9dedfcac6e16f
David Saah <dasorange.hope@gmail.com>
2023-10-20 11:40:23 +0000

    create api multiplexer

--- api/api.go
+++ api/api.go
@@ -1,1 +1,22 @@
 package api
+
+import (
+	"log"
+	"net/http"
+)
+
+type homeHandler struct{}
+
+func (h *homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Welcome to api home\n"))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Init() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.Handle("/api", &homeHandler{})
+	return mux
+}
